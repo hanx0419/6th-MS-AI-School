5fbc3e205e00a01702e7551fa8891c2e51df2736
Hans <hanx0419@naver.com>
2025-03-23 00:02:39 +0900

    AI Language Find Linked Entities

---
+++ 2025-03-10 ~ 2025-04-01/07_AI_Language_find_linked_entities.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+language_key = os.getenv("AZURE_LANGUAGE_KEY")
+language_endpoint = os.getenv("AZURE_LANGUAGE_ENDPOINT")
+
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+# Authenticate the client using your key and endpoint. 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(language_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=language_endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+# Example function for recognizing entities and providing a link to an online data source.
+def entity_linking_example(client):
+
+    try:
+        documents = [
+        """
+        Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, 
+        to develop and sell BASIC interpreters for the Altair 8800. 
+        During his career at Microsoft, Gates held the positions of chairman,
+        chief executive officer, president and chief software architect, 
+        while also being the largest individual shareholder until May 2014.
+        """
+        ]
+        result = client.recognize_linked_entities(documents = documents)[0]
+
+        print("Linked Entities:\n")
+        for entity in result.entities:
+            print("\tName: ", entity.name, "\tId: ", entity.data_source_entity_id, "\tUrl: ", entity.url,
+            "\n\tData Source: ", entity.data_source)
+            print("\tMatches:")
+            for match in entity.matches:
+                print("\t\tText:", match.text)
+                print("\t\tConfidence Score: {0:.2f}".format(match.confidence_score))
+                print("\t\tOffset: {}".format(match.offset))
+                print("\t\tLength: {}".format(match.length))
+            
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+entity_linking_example(client)
