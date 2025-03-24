65bcc0cff3328edcfa0d74e7d8d080f6a17129b1
Hans <hanx0419@naver.com>
2025-03-24 10:36:44 +0900

    Document Intelligence OCR

---
+++ 2025-03-10 ~ 2025-04-01/16_Document_Intelligence_OCR.py
@@ -0,0 +1,69 @@
+import os
+from dotenv import load_dotenv
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+import numpy as np
+
+load_dotenv()
+
+endpoint = os.getenv('DOCUMENT_INTELLIGENCE_RECOGNIZER_ENDPOINT')
+key = os.getenv('DOCUMENT_INTELLIGENCE_RECOGNIZER_KEY')
+
+def format_bounding_box(bounding_box):
+    if not bounding_box:
+        return "N/A"
+    reshaped_bounding_box = np.array(bounding_box).reshape(-1, 2)
+    return ", ".join(["[{}, {}]".format(x, y) for x, y in reshaped_bounding_box])
+
+def analyze_read():
+    # sample document
+    formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
+
+    document_intelligence_client  = DocumentIntelligenceClient(
+        endpoint=endpoint, credential=AzureKeyCredential(key)
+    )
+    
+    poller = document_intelligence_client.begin_analyze_document(
+        "prebuilt-read", AnalyzeDocumentRequest(url_source=formUrl)
+    )
+    result = poller.result()
+
+    print ("Document contains content: ", result.content)
+
+    for idx, style in enumerate(result.styles):
+        print(
+            "Document contains {} content".format(
+                "handwritten" if style.is_handwritten else "no handwritten"
+            )
+        )
+
+    for page in result.pages:
+        print("----Analyzing Read from page #{}----".format(page.page_number))
+        print(
+            "Page has width: {} and height: {}, measured with unit: {}".format(
+                page.width, page.height, page.unit
+            )
+        )
+
+        for line_idx, line in enumerate(page.lines):
+            print(
+                "...Line # {} has text content '{}' within bounding box '{}'".format(
+                    line_idx,
+                    line.content,
+                    format_bounding_box(line.polygon),
+                )
+            )
+
+        for word in page.words:
+            print(
+                "...Word '{}' has a confidence of {}".format(
+                    word.content, word.confidence
+                )
+            )
+
+    print("----------------------------------------")
+
+
+if __name__ == "__main__":
+    analyze_read()
